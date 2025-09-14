8949c7ff2369224581d539442d96e0d9f8678da4
Vinrain <143264723+vinrain-coder@users.noreply.github.com>
2025-09-14 15:08:08 +0000

    fix

--- app/checkout/checkout-form.tsx
+++ app/checkout/checkout-form.tsx
@@ -689,11 +689,11 @@ const CheckoutForm = () => {
                   {paymentMethod === "Paystack" ? (
                     <PaystackInline
                       email={session?.user.email as string}
-                      amount={Math.round(totalPrice * 100)} // Paystack wants kobo
+                      amount={Math.round(totalPrice * 100)} 
                       publicKey={process.env.NEXT_PUBLIC_PAYSTACK_PUBLIC_KEY!}
-                      orderId={createdOrder._id}
+                      orderId={createdOrder.id}
                       onSuccess={() =>
-                        router.push(`/account/orders/${createdOrder._id}`)
+                        router.push(`/account/orders/${createdOrder.id}`)
                       }
                     />
                   ) : (
