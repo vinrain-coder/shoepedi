355c82ed29dc6dc6da81bc928eb737ea7e6d4498
Vinrain <143264723+vinrain-coder@users.noreply.github.com>
2025-09-24 16:20:09 +0000

    edit site header

--- app/admin/site-header.tsx
+++ app/admin/site-header.tsx
@@ -4,14 +4,15 @@ import { SidebarTrigger } from "@/components/ui/sidebar";
 
 export function SiteHeader() {
   return (
-    <header className="flex h-(--header-height) shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)">
+    <header className="flex h-(--header-height) shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear">
       <div className="flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6">
-        <SidebarTrigger className="-ml-1 text-2xl" />
-
-        <div className="hidden sm:flex">
-          <Button variant="ghost" size="sm">
-            <ThemeSwitcher />
-          </Button>
+        <div className="ml-auto flex items-center gap-2">
+          <SidebarTrigger className="text-2xl" />
+          <div className="hidden sm:flex">
+            <Button variant="ghost" size="sm">
+              <ThemeSwitcher />
+            </Button>
+          </div>
         </div>
       </div>
     </header>
