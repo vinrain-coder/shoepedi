f29b13a1e926fb87a4f9b3598cfd1069165900d8
Vinrain <143264723+vinrain-coder@users.noreply.github.com>
2025-09-24 15:22:29 +0000

    Fix react

--- app/admin/main-nav.tsx
+++ app/admin/main-nav.tsx
@@ -31,9 +31,17 @@ export function NavMain({
               tooltip="Quick Create"
               className="bg-primary text-primary-foreground hover:bg-primary/90 hover:text-primary-foreground active:bg-primary/90 active:text-primary-foreground min-w-8 duration-200 ease-linear"
             >
-              <IconCirclePlusFilled />
-              <Link href="/admin/products/create">Create Product</Link>
+              <Link
+                href="/admin/products/create"
+                className="flex items-center gap-2"
+              >
+                <div className="flex items-center gap-2">
+                  <IconCirclePlusFilled />
+                  <span>Create Product</span>
+                </div>
+              </Link>
             </SidebarMenuButton>
+
             <Button
               size="icon"
               className="size-8 group-data-[collapsible=icon]:opacity-0"
