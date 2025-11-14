83a057df5fd0a07c4041674af9edbc6c0e822d1f
Vinrain <143264723+vinrain-coder@users.noreply.github.com>
2025-11-14 16:13:59 +0300

    Adjust padding in search results layout
    Updated padding for results header and product grid.

--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -173,7 +173,7 @@ export default function SearchPage() {
       {/* --------------------------- */}
       {/* RESULTS HEADER */}
       {/* --------------------------- */}
-      <div className="my-2 bg-card flex flex-col md:flex-row items-start md:items-center justify-between p-3 border-b">
+      <div className="my-2 bg-card flex flex-col md:flex-row items-start md:items-center justify-between p-0 border-b">
         <div className="flex flex-wrap items-center gap-2">
           <div>
             {totalProducts === 0
@@ -473,7 +473,7 @@ export default function SearchPage() {
         {/* --------------------------- */}
         {/* PRODUCT GRID */}
         {/* --------------------------- */}
-        <div className="md:col-span-4 space-y-4 p-3">
+        <div className="md:col-span-4 space-y-4 p-0">
           <div>
             <div className="font-bold text-xl">Results</div>
             <div>Check each product page for other buying options</div>
