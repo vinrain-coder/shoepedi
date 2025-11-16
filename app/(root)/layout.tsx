e82846b8da321155924b6347fe6177496d27d143
Vinrain <143264723+vinrain-coder@users.noreply.github.com>
2025-11-16 08:19:58 +0300

    Adjust padding in main layout component

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -11,7 +11,7 @@ export default async function RootLayout({
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
-      <main className="flex-1 flex flex-col p-4">{children}</main>
+      <main className="flex-1 flex flex-col py-4 px-3">{children}</main>
       <Footer />
     </div>
   );
