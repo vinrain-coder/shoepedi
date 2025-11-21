7cb8da426fcce914a1017efc1114828a1fc9f199
Vinrain <143264723+vinrain-coder@users.noreply.github.com>
2025-11-21 10:37:09 +0000

    update order details page

--- app/(root)/account/orders/[id]/page.tsx
+++ app/(root)/account/orders/[id]/page.tsx
@@ -1,11 +1,9 @@
 import { notFound } from "next/navigation";
-import React from "react";
-
-import { auth } from "@/auth";
 import { getOrderById } from "@/lib/actions/order.actions";
 import OrderDetailsForm from "@/components/shared/order/order-details-form";
-import Link from "next/link";
 import { formatId } from "@/lib/utils";
+import { getServerSession } from "@/lib/get-session";
+import Breadcrumb from "@/components/shared/breadcrumb";
 
 export async function generateMetadata(props: {
   params: Promise<{ id: string }>;
@@ -29,22 +27,16 @@ export default async function OrderDetailsPage(props: {
   const order = await getOrderById(id);
   if (!order) notFound();
 
-  const session = await auth();
+  const session = await getServerSession();
 
   return (
-    <>
-      <div className="flex gap-2">
-        <Link href="/account">Your Account</Link>
-        <span>›</span>
-        <Link href="/account/orders">Your Orders</Link>
-        <span>›</span>
-        <span>Order {formatId(order._id)}</span>
-      </div>
+    <div>
+      <Breadcrumb />
       <h1 className="h1-bold py-4">Order {formatId(order._id)}</h1>
       <OrderDetailsForm
         order={order}
         isAdmin={session?.user?.role === "Admin" || false}
       />
-    </>
+    </div>
   );
 }
