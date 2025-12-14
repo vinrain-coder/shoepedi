4c6362aae2d4e5bb04b82a98e6a0ac920a8e27db
Vinrain <143264723+vinrain-coder@users.noreply.github.com>
2025-12-14 12:53:28 +0300

    Update loading.tsx

--- app/(root)/page/[slug]/loading.tsx
+++ app/(root)/page/[slug]/loading.tsx
@@ -3,32 +3,38 @@ import { Skeleton } from "@/components/ui/skeleton";
 export default function Loading() {
   return (
     <div className="p-1 sm:p-2 md:p-8 max-w-3xl mx-auto">
-      {/* Breadcrumb */}
-      <Skeleton className="block h-4 w-48 mb-4" />
+      {/* Breadcrumb skeleton */}
+      <div className="flex gap-2 mb-4">
+        <Skeleton className="h-4 w-20" />
+        <Skeleton className="h-4 w-4" />
+        <Skeleton className="h-4 w-24" />
+      </div>
 
-      {/* Title */}
-      <Skeleton className="block h-9 w-3/4 pb-4" />
+      {/* Title skeleton */}
+      <Skeleton className="h-10 w-3/4 mb-6" />
 
-      {/* Content */}
-      <section>
-        <div className="space-y-2">
-          <Skeleton className="block h-4 w-full" />
-          <Skeleton className="block h-4 w-11/12" />
-          <Skeleton className="block h-4 w-10/12" />
+      {/* Markdown content skeleton */}
+      <section className="space-y-4">
+        <Skeleton className="h-4 w-full" />
+        <Skeleton className="h-4 w-full" />
+        <Skeleton className="h-4 w-5/6" />
 
-          <div className="h-3" />
+        <Skeleton className="h-4 w-full mt-6" />
+        <Skeleton className="h-4 w-11/12" />
+        <Skeleton className="h-4 w-4/5" />
 
-          <Skeleton className="block h-4 w-full" />
-          <Skeleton className="block h-4 w-9/12" />
-          <Skeleton className="block h-4 w-11/12" />
+        {/* Simulate heading */}
+        <Skeleton className="h-6 w-1/2 mt-8" />
 
-          <div className="h-3" />
+        <Skeleton className="h-4 w-full" />
+        <Skeleton className="h-4 w-full" />
+        <Skeleton className="h-4 w-2/3" />
 
-          <Skeleton className="block h-4 w-full" />
-          <Skeleton className="block h-4 w-10/12" />
-        </div>
+        <Skeleton className="h-4 w-full" />
+        <Skeleton className="h-4 w-full" />
+        <Skeleton className="h-4 w-5/6" />
       </section>
     </div>
   );
-      }
+}
       
