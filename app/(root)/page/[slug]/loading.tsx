6320a86511d74c24f15d99b66b8e89affe3b2f5f
Vinrain <143264723+vinrain-coder@users.noreply.github.com>
2025-12-14 12:33:43 +0300

    Update loading.tsx

--- app/(root)/page/[slug]/loading.tsx
+++ app/(root)/page/[slug]/loading.tsx
@@ -2,31 +2,32 @@ import { Skeleton } from "@/components/ui/skeleton";
 
 export default function Loading() {
   return (
-    <div className="max-w-3xl mx-auto px-1 sm:px-2 md:px-4 space-y-6">
+    <div className="p-1 sm:p-2 md:p-8 max-w-3xl mx-auto">
       {/* Breadcrumb */}
-      <Skeleton className="h-4 w-48" />
+      <Skeleton className="h-4 w-48 mb-4" />
 
       {/* Title */}
-      <Skeleton className="h-9 w-3/4" />
+      <Skeleton className="h-9 w-3/4 pb-4" />
 
-     {/* Markdown Content */}
-      <div className="space-y-2">
-        <Skeleton className="h-4 w-full" />
-        <Skeleton className="h-4 w-11/12" />
-        <Skeleton className="h-4 w-10/12" />
+      {/* Content */}
+      <section>
+        <div className="space-y-2">
+          <Skeleton className="h-4 w-full" />
+          <Skeleton className="h-4 w-11/12" />
+          <Skeleton className="h-4 w-10/12" />
 
-        <div className="h-3" />
+          <div className="h-3" />
 
-        <Skeleton className="h-4 w-full" />
-        <Skeleton className="h-4 w-9/12" />
-        <Skeleton className="h-4 w-11/12" />
+          <Skeleton className="h-4 w-full" />
+          <Skeleton className="h-4 w-9/12" />
+          <Skeleton className="h-4 w-11/12" />
 
-        <div className="h-3" />
+          <div className="h-3" />
 
-        <Skeleton className="h-4 w-full" />
-        <Skeleton className="h-4 w-10/12" />
-      </div>
+          <Skeleton className="h-4 w-full" />
+          <Skeleton className="h-4 w-10/12" />
+        </div>
+      </section>
     </div>
   );
 }
-        
