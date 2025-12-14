e6d61235816231b459c8112c92ecc6b4b72a42d8
Vinrain <143264723+vinrain-coder@users.noreply.github.com>
2025-12-14 11:34:01 +0300

    Update page.tsx

--- app/(root)/blogs/page.tsx
+++ app/(root)/blogs/page.tsx
@@ -40,12 +40,7 @@ async function BlogList({ currentPage }: { currentPage: number }) {
 
 function BlogListSkeleton() {
   return (
-    <div className="space-y-4">
-      <div>
-        <Skeleton className="h-7 w-40" />
-        <Skeleton className="h-5 w-64 mt-2" />
-      </div>
-
+    <div className="space-y-6">
       <div className="grid grid-cols-2 gap-4 md:grid-cols-3 lg:grid-cols-4">
         {Array.from({ length: 8 }).map((_, i) => (
           <div key={i} className="space-y-2">
