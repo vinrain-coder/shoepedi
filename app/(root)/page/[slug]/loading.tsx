b52a3ec6af8b3600e9e1571c845e27d1cd568aec
Vinrain <143264723+vinrain-coder@users.noreply.github.com>
2025-12-14 12:43:23 +0300

    Update loading.tsx

--- app/(root)/page/[slug]/loading.tsx
+++ app/(root)/page/[slug]/loading.tsx
@@ -4,30 +4,31 @@ export default function Loading() {
   return (
     <div className="p-1 sm:p-2 md:p-8 max-w-3xl mx-auto">
       {/* Breadcrumb */}
-      <Skeleton className="h-4 w-48 mb-4" />
+      <Skeleton className="block h-4 w-48 mb-4" />
 
       {/* Title */}
-      <Skeleton className="h-9 w-3/4 pb-4" />
+      <Skeleton className="block h-9 w-3/4 pb-4" />
 
       {/* Content */}
       <section>
         <div className="space-y-2">
-          <Skeleton className="h-4 w-full" />
-          <Skeleton className="h-4 w-11/12" />
-          <Skeleton className="h-4 w-10/12" />
+          <Skeleton className="block h-4 w-full" />
+          <Skeleton className="block h-4 w-11/12" />
+          <Skeleton className="block h-4 w-10/12" />
 
           <div className="h-3" />
 
-          <Skeleton className="h-4 w-full" />
-          <Skeleton className="h-4 w-9/12" />
-          <Skeleton className="h-4 w-11/12" />
+          <Skeleton className="block h-4 w-full" />
+          <Skeleton className="block h-4 w-9/12" />
+          <Skeleton className="block h-4 w-11/12" />
 
           <div className="h-3" />
 
-          <Skeleton className="h-4 w-full" />
-          <Skeleton className="h-4 w-10/12" />
+          <Skeleton className="block h-4 w-full" />
+          <Skeleton className="block h-4 w-10/12" />
         </div>
       </section>
     </div>
   );
-}
+      }
+      
