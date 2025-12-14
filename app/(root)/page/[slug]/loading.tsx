7b9251c23ba1f9226da0bd8173395e75687bd8bb
Vinrain <143264723+vinrain-coder@users.noreply.github.com>
2025-12-14 12:15:02 +0300

    Update loading.tsx

--- app/(root)/page/[slug]/loading.tsx
+++ app/(root)/page/[slug]/loading.tsx
@@ -2,19 +2,31 @@ import { Skeleton } from "@/components/ui/skeleton";
 
 export default function Loading() {
   return (
-   <div className="p-1 sm:p-2 md:p-8 max-w-3xl mx-auto">
-      {/* Title */}
-      <Skeleton className="h-10 w-3/4 pb-4" />
+    <div className="p-1 sm:p-2 md:p-8 max-w-3xl mx-auto space-y-6">
+      {/* Breadcrumb */}
+      <Skeleton className="h-4 w-48" />
 
-      {/* Content */}
-      <div className="space-y-2">
-        <Skeleton className="h-4 w-full" />
+      {/* Title */}
+      <Skeleton className="h-9 w-3/4" />
+
+      {/* Content (Markdown-like blocks) */}
+      <div className="space-y-3">
         <Skeleton className="h-4 w-full" />
         <Skeleton className="h-4 w-11/12" />
         <Skeleton className="h-4 w-10/12" />
+
+        <div className="h-3" />
+
         <Skeleton className="h-4 w-full" />
         <Skeleton className="h-4 w-9/12" />
+        <Skeleton className="h-4 w-11/12" />
+
+        <div className="h-3" />
+
+        <Skeleton className="h-4 w-full" />
+        <Skeleton className="h-4 w-10/12" />
       </div>
     </div>
   );
-}
+      }
+        
