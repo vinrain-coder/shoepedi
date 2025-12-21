e5434d717a7dcb1f5ef52f002b5558342446d790
Vinrain <143264723+vinrain-coder@users.noreply.github.com>
2025-12-21 11:32:23 +0300

    Update validator.ts

--- lib/validator.ts
+++ lib/validator.ts
@@ -325,11 +325,7 @@ export const SettingInputSchema = z.object({
 export const BlogInputSchema = z.object({
   title: z.string().min(3, "Title must be at least 3 characters"),
   slug: z.string().min(3, "Slug must be at least 3 characters"),
-  image: z
-  .string()
-  .url()
-  .optional()
-  .transform((val) => (val?.trim() ? val : undefined)),  
+   image: z.string().url().optional().or(z.literal("")), 
   content: z.string().min(10, "Content must be at least 10 characters"),
   category: z.string().min(3, "Category is required"),
   views: z.number(),
