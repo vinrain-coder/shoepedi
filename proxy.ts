2cb5c8034fc3e16e395b3ec0044cb97810a1cbc5
vinrain <143264723+vinrain-coder@users.noreply.github.com>
2026-01-17 14:41:56 +0000

    fix proxy.ts

--- proxy.ts
+++ proxy.ts
@@ -58,7 +58,6 @@ export function proxy(req: NextRequest) {
 
 export const config = {
   matcher: [
-    "/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).
-        *)",
+    "/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)",
   ],
 };
