d7cb356f6857e139acf8c930d09c021dbff10d3b
Vinrain <143264723+vinrain-coder@users.noreply.github.com>
2026-02-19 15:22:08 +0300

    Change header to sticky with background color

--- app/admin/site-header.tsx
+++ app/admin/site-header.tsx
@@ -4,7 +4,7 @@ import { SidebarTrigger } from "@/components/ui/sidebar";
 
 export function SiteHeader() {
   return (
-    <header className="flex h-(--header-height) shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)">
+    <header className="sticky top-0 z-50 flex h-(--header-height) shrink-0 items-center gap-2 border-b bg-background transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)">
       <div className="flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6">
         <SidebarTrigger className="-ml-1 !w-[32px] !h-[32px]" />
         <div className="flex ml-auto">
